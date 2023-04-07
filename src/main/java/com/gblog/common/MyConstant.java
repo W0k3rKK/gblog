a633d8804254cd837402741b90ae2d6304ea8a99
W0k3rKK <570488980@qq.com>
2023-04-07 15:55:34 +0800

    将认证和授权从shiro换为spring security

---
+++ src/main/java/com/gblog/common/MyConstant.java
@@ -0,0 +1,11 @@
+package com.gblog.common;
+
+/**
+ * @author: W0k3rKK
+ * @description
+ * @date: 2023/4/7 15:45
+ */
+public class MyConstant {
+	public static final String JWT_SIGN_KEY = "gblog";
+
+}
